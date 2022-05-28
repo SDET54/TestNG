23d0867d5ea487d60ffdd988901fe39773975f09
Vasif Amrahov <mr.vasif.amrahov@gmail.com>
2022-05-28 12:31:11 +0400

    28.05.2022

--- src/test/java/tests/AmazonIlkTest.java
+++ src/test/java/tests/AmazonIlkTest.java
@@ -3,6 +3,7 @@ package tests;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.testng.annotations.Test;
 import pages.AmazonPage;
 import utilities.Driver;
@@ -16,20 +17,30 @@ public class AmazonIlkTest {
 
     @Test
     public void test01() throws IOException {
+
+        //amazon sayfasina git
         Driver.getDriver().get("https://www.amazon.com");
 
-        AmazonPage amazonPage = new AmazonPage();
-        amazonPage.aramaKutusuElementi.sendKeys("Laptop" + Keys.ENTER);
-
+        //tum sayfa ekran goruntusu al
         LocalDateTime date = LocalDateTime.now();
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyMMddHHmmss");
         String tarih = date.format(dtf);
 
-        File aramaSonucSS = new File("target/amazon/aramaSonuc/screenShot" + tarih + ".jpeg");
-        File temp = amazonPage.aramaSonucElementi.getScreenshotAs(OutputType.FILE);
+        TakesScreenshot ts = (TakesScreenshot) Driver.getDriver();
+        File tumSayfaSS = new File("target/amazon/tumSayfaSS/screenShot" + tarih + ".jpeg");
+        File temp = ts.getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(temp,tumSayfaSS);
 
+        //arama kutusuna Laptop yaz ve ara
+        AmazonPage amazonPage = new AmazonPage();
+        amazonPage.aramaKutusuElementi.sendKeys("Laptop" + Keys.ENTER);
+
+        //arama sonucu yazisinin ekran goruntusunu al
+        File aramaSonucSS = new File("target/amazon/aramaSonucSS/screenShot" + tarih + ".jpeg");
+        temp = amazonPage.aramaSonucElementi.getScreenshotAs(OutputType.FILE);
         FileUtils.copyFile(temp, aramaSonucSS);
 
+        //sayfayi kapat
         Driver.closeDriver();
     }
 }
